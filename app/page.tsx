7eb93d065e49a7e1e4db1a21639c971c4cfc08ee
luirapero-web <lui.rapero@gmail.com>
2026-09-11 01:11:06 -0500

    Actualizar page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-  import Script from "next/script" 
+import Script from "next/script"
 
-  export default function Page() {
-    return (
+export default function Page() {
+  return (
     <main style={{background:"black",color:"white",padding:"20px",maxWidth:"500px",margin:"0 auto",fontFamily:"sans-serif"}}>
-      <h1>ACA - CLASIFICATORIA 08</h1> 
+      <h1>ACA - CLASIFICATORIA 08</h1>
       <p>Domingo 13 - Campo de Marte - s/5</p>
       <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DdH-joVALUn/" data-instgrm-version="14">
         <a href="https://www.instagram.com/p/DdH-joVALUn/">Ver flyer</a>
-    </blockquote>
-    <Script src="//www.instagram.com/embed.js" async />
-    </main> }
-)
-  }
+      </blockquote>
+      <Script src="//www.instagram.com/embed.js" async />
+    </main>
+  )
+}
