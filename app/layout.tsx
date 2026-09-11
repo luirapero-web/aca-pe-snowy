cf7f3527d84eded7133729a7695c9ab5f23404dd
luirapero-web <lui.rapero@gmail.com>
2026-09-11 02:42:31 -0500

    Actualizar layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css"
-export const metadata = { title: "ACA - Hip Hop Underground" }
+export const metadata = { title: "ACA.PE - ¿Dónde es hoy?" }
 export default function RootLayout({children}:{children:any}){
-  return <html lang="es"><body>{children}</body></html>
+  return (
+    <html lang="es">
+      <head><script src="https://cdn.tailwindcss.com"></script></head>
+      <body>{children}</body>
+    </html>
+  )
 }
